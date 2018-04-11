1a81aa50b0a8abeed3716db77d4814f531db1702
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-04-11 14:48:39 +0200

    More edits to support VPC firewall rules

--- lib/chef/knife/cs_firewallrule_create.rb
+++ lib/chef/knife/cs_firewallrule_create.rb
@@ -88,14 +88,31 @@ module KnifeCloudstack
       startport = args[0]
       endport   = args[1] || args[0]
       protocol  = args[2] || "TCP"
-      cidrlist  = args[3] || "0.0.0.0/0"      
+      cidrlist  = args[3] || "0.0.0.0/0"
 
       # Required parameters
       params = {
-        'command' => 'createFirewallRule',
         'ipaddressId' => ip_address['id'],
         'protocol' => protocol
       }
+      
+      if config[:public_ip].nil?
+        params['command'] = 'createFirewallRule'
+      else
+        params['command'] = 'createNetworkACL'
+        # Random rule number; will be temp and hopefully wont hit collision
+        params['number'] = (0...4).map { [rand(10)] }.join
+        params['action'] = 'Allow'
+        params['traffictype'] = 'Ingress'
+        # To keep the def backwards compatible..
+        hostname = @name_args.first
+
+        server = connection.get_server(hostname)
+        server_nic_default = connection.get_server_default_nic(server)
+        networkid = server_nic_default['networkid']
+        # Just assume we want this on the first (only?) acl for now:
+        params['aclid'] = connection.get_networkAcls(networkid).first
+      end
 
       # Optional parameters
       opt_params = {
@@ -103,7 +120,7 @@ module KnifeCloudstack
         'endport' => endport,
         'cidrlist' => cidrlist
       }
-  
+
       params.merge!(opt_params)
  
       Chef::Log.debug("Creating Firewall Rule for
