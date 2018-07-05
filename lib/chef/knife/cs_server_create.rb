84db1d91e38b245ce856e8f519b2f5504560bdd6
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-07-05 10:29:19 +0200

    Bugfix

--- lib/chef/knife/cs_server_create.rb
+++ lib/chef/knife/cs_server_create.rb
@@ -238,7 +238,7 @@ module KnifeCloudstack
            :default => false
 
     option :startvm,
-           :long => '--[no-]startvm',
+           :long => "--[no-]startvm",
            :description => "Specify whether the VM should be started after creation.",
            :boolean => true,
            :default => true
@@ -298,7 +298,7 @@ module KnifeCloudstack
       params['displayname'] = if locate_config_value :set_display_name and locate_config_value :chef_node_name then locate_config_value :chef_node_name else hostname end
       params['ipaddress'] = locate_config_value(:ik_private_ip) if locate_config_value(:ik_private_ip)
       params['size'] = locate_config_value(:size) if locate_config_value(:size)
-      params['startvm'] = locate_config_value(:startvm) if locate_config_value(:startvm)
+      params['startvm'] = locate_config_value(:startvm).nil? ? true : locate_config_value(:startvm)
 
       @server = connection.create_server(
           hostname,
