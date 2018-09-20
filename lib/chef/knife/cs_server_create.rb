d36551fabf1cf0342f822cec129d6d5941bda462
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-20 20:54:30 +0200

    Parse correct value for startvm to API

--- lib/chef/knife/cs_server_create.rb
+++ lib/chef/knife/cs_server_create.rb
@@ -305,7 +305,8 @@ module KnifeCloudstack
       params['displayname'] = if locate_config_value :set_display_name and locate_config_value :chef_node_name then locate_config_value :chef_node_name else hostname end
       params['ipaddress'] = locate_config_value(:ik_private_ip) if locate_config_value(:ik_private_ip)
       params['size'] = locate_config_value(:size) if locate_config_value(:size)
-      params['startvm'] = config[:startvm]
+      params['startvm'] = 'true'
+      params['startvm'] = config[:startvm].to_s if config[:startvm]
       params['diskcontroller'] = locate_config_value(:cloudstack_diskcontroller) if locate_config_value(:cloudstack_diskcontroller)
 
       @server = connection.create_server(
