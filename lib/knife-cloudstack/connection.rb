039508ba71c2dbb1797c03231eb057d4fcba4431
Robbert-Jan Sperna Weiland <rspernaweiland@schubergphilis.com>
2018-09-21 22:11:48 +0200

    Cleanup

--- lib/knife-cloudstack/connection.rb
+++ lib/knife-cloudstack/connection.rb
@@ -270,8 +270,6 @@ module CloudstackClient
       params['diskOfferingId'] = disk['id'] if disk
 
       json = send_async_request(params)
-      require 'pp'
-      pp json
       json['virtualmachine']
     end
 
